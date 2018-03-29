f4cb34667fa05ea33da11932ea03a52280abccad
pranav <pranavdixit08@gmail.com>
2018-03-29 15:41:30 -0700

    remove search, as you have removed the button

--- scripts.js
+++ scripts.js
@@ -61,40 +61,6 @@ document.getElementById("sayButton").onclick = function(){
 
 }
 
-document.getElementById("searchButton").onclick = function(){
-
-	var postId = $('#postId').val();
-
-
-	$.ajax({
-				url: API_ENDPOINT + '?postId='+postId,
-				type: 'GET',
-				success: function (response) {
-
-					$('#posts tr').slice(1).remove();
-
-	        jQuery.each(response, function(i,data) {
-
-						var player = "<audio controls><source src='" + data['url'] + "' type='audio/mpeg'></audio>"
-
-						if (typeof data['url'] === "undefined") {
-	    				var player = ""
-						}
-
-						$("#posts").append("<tr> \
-								<td>" + data['id'] + "</td> \
-								<td>" + data['voice'] + "</td> \
-								<td>" + data['text'] + "</td> \
-								<td>" + data['status'] + "</td> \
-								<td>" + player + "</td> \
-								</tr>");
-	        });
-				},
-				error: function () {
-						alert("error");
-				}
-		});
-}
 
 document.getElementById("postText").onkeyup = function(){
 	var length = $(postText).val().length;
